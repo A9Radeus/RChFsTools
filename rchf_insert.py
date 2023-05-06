ca302891313f27a87374f6bfb205ffe311a9e8a2
Radeus <zelzelu9@o2.pl>
2023-05-06 22:11:59 +0200

    Better args parsing. Add silent mode.

--- rchf_insert.py
+++ rchf_insert.py
@@ -3,28 +3,23 @@
 # [x] Allow both prefixes, suffxies
 #       [x] Prefixes
 #       [x] Suffixes
-# [ ] Use library for handling CLI flags, etc
+# [x] Use library for handling CLI flags, etc
 # [ ] Option (prolly a flag) that gives user a preview of N first files and how they would change if application
 #     runs with given arguments
-# [ ] Option (prolly a flag): silent mode -- no "Done" at the end, etc
+# [x] Option (prolly a flag): silent mode -- no "Done" at the end, etc
 # ===========================================================================================================
 
 import os
-import sys
+import argparse
 
-def get_files_in_dir_ext(dir_path_str, allowed_exts):
-    result = []
-    dir = os.fsencode(dir_path_str)
-    for file in os.listdir(dir):
-        filename = os.fsdecode(file)
-        for ext in allowed_exts:
-            if filename.endswith(ext):
-                result.append(os.path.join(dir, file))
-                break
-    return result
+class ConsolePrinter:
+    def __init__(self, app_args):
+        self.app_args = app_args
+    
+    def cprint(self, msg):
+        if self.app_args.quiet == False:
+            print(msg)
 
-# TODO: This could be more pythonic
-# TODO: Add global/relative path option as well
 def get_files_in_dir(dir_path_str, only_names = False):
     result = []
     dir = os.fsencode(dir_path_str)
@@ -35,43 +30,28 @@ def get_files_in_dir(dir_path_str, only_names = False):
             result.append(os.path.join(dir, file))     
     return result
 
-if __name__ == '__main__':
-    if len(sys.argv) < 4:
-        print('Wrong number of arguments. Correct syntax: [prog_name] [path_to_target_dir] [text_to_insert] [flags]')
-        exit(-1)
-
-    input_path_str = str(sys.argv[1])
+def run_main_app(app_args):
+    input_path_str = str(app_args.dir_path)
     input_path = os.fsencode(input_path_str)
+    to_insert = str(app_args.str_to_insert)
 
     files_in_dir = get_files_in_dir(input_path_str, True)
-    str_paths = [os.fsdecode(p) for p in files_in_dir]
-
-    to_insert = str(sys.argv[2])
-
-    input_insert_prefix = False
-    input_insert_postfix = False
-
-    for i in range(3, len(sys.argv)):
-        if str(sys.argv[i]) == '-s' or str(sys.argv[i]) == '--start':
-            input_insert_prefix = True
-        if str(sys.argv[i]) == '-e' or str(sys.argv[i]) == '--end':
-            input_insert_postfix = True
 
     if len(files_in_dir) == 0:
-        print('No files in given directory.')
-        exit(-2)
+        printer.cprint('No files in given directory.')
+        return
 
     for file in files_in_dir:
         no_ext, ext = os.path.splitext(file)
         
         str_file = ''
 
-        if input_insert_prefix:
+        if app_args.start:
             str_file += to_insert
         
         str_file += os.fsdecode(no_ext)
 
-        if input_insert_postfix:
+        if app_args.end:
             str_file += to_insert
         
         str_file += os.fsdecode(ext)
@@ -80,4 +60,22 @@ if __name__ == '__main__':
         new_path = os.path.join(input_path, os.fsencode(str_file))
         os.rename(old_path, new_path)
 
-    print('Done.')
+
+if __name__ == '__main__':
+    input_parser = argparse.ArgumentParser()
+    input_parser.add_argument("dir_path")
+    input_parser.add_argument("str_to_insert")
+    input_parser.add_argument("-s", "--start", action="store_true")
+    input_parser.add_argument("-e", "--end", action="store_true")
+    input_parser.add_argument("-q", "--quiet", action="store_true")
+    args = input_parser.parse_args()
+
+    if args.start == False and args.end == False:
+        print('Specify at least one of:  \'--start\', \'--end\'')
+        raise SystemExit(1)
+
+    printer = ConsolePrinter(args)
+
+    run_main_app(args)
+    
+    printer.cprint('Done.')
